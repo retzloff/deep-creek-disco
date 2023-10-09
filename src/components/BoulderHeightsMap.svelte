376f3d4fddc22b26d1a14413a4053be380c6d3f4
Eric Retzloff <eretzloff@gmail.com>
2023-10-08 23:04:32 -0400

    :art: Link to the address, not the rental property

--- src/components/BoulderHeightsMap.svelte
+++ src/components/BoulderHeightsMap.svelte
@@ -1,10 +1,10 @@
 <iframe
 	title="Map of Boulder Heights"
-	src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d15309.562169430215!2d-79.36952379798856!3d39.5449095464024!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89caab0222475e1d%3A0xabe302cc6cba2b91!2sBoulder%20heights!5e0!3m2!1sen!2sus!4v1696814228328!5m2!1sen!2sus"
+	src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d23940.978392526064!2d-79.38107349762876!3d39.54255026593978!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89caab3bac9300b9%3A0xc46ea6d7845a5ad0!2s105%20Boulder%20Ridge%20Dr%2C%20McHenry%2C%20MD%2021541!5e0!3m2!1sen!2sus!4v1696820567218!5m2!1sen!2sus"
 	width="480"
 	height="270"
 	style="border:0;"
-	allow="fullscreen="
+	allow="fullscreen"
 	loading="lazy"
 	referrerpolicy="no-referrer-when-downgrade"
 	class="rounded shadow-lg"
