72e8fd300fa9c5ab826746a7d5ca884fcba28783
Eric Retzloff <eretzloff@gmail.com>
2023-10-15 18:45:07 -0400

    :memo: Adds checkin/out hours

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -9,9 +9,13 @@
 </div>
 
 <section class="text-slate-200 bg-cloud-burst">
-	<img src="./house-front.jpg" class="contrast-75" alt="" />
-	<h1 class="">Deep Creek Bears</h1>
-	<p class="">October 26th - 30th, 2023</p>
+	<div class="">
+		<img src="./house-front.jpg" class="contrast-75" alt="" />
+	</div>
+	<div>
+		<h1>Deep Creek Bears</h1>
+		<p class="text-xl">October 26th - 30th, 2023</p>
+	</div>
 </section>
 
 <section class="bg-anzac light">
@@ -29,7 +33,9 @@
 </section>
 
 <section class="bg-selective-yellow light">
-	<h2>Location</h2>
+	<h2>The House</h2>
+	<p>Check-in: Thursday at 4pm.</p>
+	<p>Check-out: Monday at 12pm.</p>
 	<address>
 		<div class="pb-2">105 Boulder Ridge Dr, McHenry, MD 21541</div>
 		<BoulderHeightsMap />
