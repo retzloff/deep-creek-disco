289b4753d7143245386cc27cc92db53da2734e71
Eric Retzloff <eretzloff@gmail.com>
2023-10-31 20:36:16 -0400

    :sparkles: 2024 Initial commit

--- src/routes/(main)/+page.svelte
+++ src/routes/(main)/+page.svelte
@@ -1,35 +1,9 @@
 <script>
-	import CountdownTimer from '../../components/CountdownTimer.svelte';
-	import BoulderHeightsMap from '../../components/BoulderHeightsMap.svelte';
-	import MealList from '../../components/MealList.svelte';
 </script>
 
-<section class="text-slate-200 bg-cloud-burst">
-	<div class="">
-		<img src="./house-front.jpg" class="contrast-75" alt="" />
+<div class="h-screen flex flex-col justify-center">
+	<div class="py-8 px-16 space-y-2 text-white bg-orange-500">
+		<h1 class="text-4xl font-semibold">Deep Creek Bears 2024</h1>
+		<p class="text-xl">More info coming soon...</p>
 	</div>
-	<div>
-		<h1>Deep Creek Bears</h1>
-		<p class="text-xl">October 26th - 30th, 2023</p>
-	</div>
-</section>
-
-<section class="bg-anzac light">
-	<h2>Countdown</h2>
-	<CountdownTimer />
-</section>
-
-<section class="bg-sirocco">
-	<h2>The House</h2>
-	<p>Check-in: Thursday after 4pm.</p>
-	<p>Check-out: Monday before 11am.</p>
-	<address>
-		<div class="pb-2">105 Boulder Ridge Dr, McHenry, MD 21541</div>
-		<BoulderHeightsMap />
-	</address>
-</section>
-
-<section class="bg-anzac light">
-	<h2>Meals</h2>
-	<MealList />
-</section>
+</div>
