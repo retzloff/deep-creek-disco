c8b178b0b7b018de0241a3937bcd314a309f0da9
Eric Retzloff <eretzloff@gmail.com>
2023-10-31 20:39:15 -0400

    :lipstick: Mobile fixes

--- src/routes/(main)/+page.svelte
+++ src/routes/(main)/+page.svelte
@@ -2,8 +2,8 @@
 </script>
 
 <div class="h-screen flex flex-col justify-center">
-	<div class="py-8 px-16 space-y-2 text-white bg-orange-500">
-		<h1 class="text-4xl font-semibold">Deep Creek Bears 2024</h1>
+	<div class="py-8 px-8 md:px-16 space-y-2 text-white bg-orange-500">
+		<h1 class="text-3xl md:text-4xl font-semibold">Deep Creek Bears 2024</h1>
 		<p class="text-xl">More info coming soon...</p>
 	</div>
 </div>
