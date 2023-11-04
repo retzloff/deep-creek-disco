8b9cd8eb09ff92aab32528bbc65a352ace8536a4
Eric Retzloff <eretzloff@gmail.com>
2023-11-04 14:39:45 -0400

    :pencil2: Add dates

--- src/routes/(main)/+page.svelte
+++ src/routes/(main)/+page.svelte
@@ -1,8 +1,14 @@
 <div class="h-screen flex flex-col justify-center">
 	<div
-		class="border border-y border-orange-800 py-8 px-8 md:px-16 space-y-2 text-white bg-orange-500 shadow-xl"
+		class="mx-auto border border-y border-orange-800 py-8 px-4 md:px-8 space-y-2 text-white bg-gradient-to-br from-orange-500 to-orange-800 shadow-xl max-w-lg rounded-xl text-center"
 	>
-		<h1 class="text-3xl md:text-4xl font-semibold">Deep Creek Bears 2024</h1>
-		<p class="text-xl">More info coming soon...</p>
+		<div class="border-b-2 border-white/50 pb-4 px-4">
+			<h1 class="text-3xl md:text-4xl font-semibold">Deep Creek Bears 2024</h1>
+			<h2 class="text-xl">October 24 - 28, 2024</h2>
+		</div>
+		<p class="pt-4 text-2xl font-bold text-white/90 capitalize italic">Save the date!</p>
 	</div>
 </div>
+
+<style>
+</style>
