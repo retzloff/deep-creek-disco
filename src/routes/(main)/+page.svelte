2b1e7fc1fa0f3f78987fa71d2e83253fab1ea91c
Eric Retzloff <eretzloff@gmail.com>
2023-11-03 20:31:13 -0400

    Shadow

--- src/routes/(main)/+page.svelte
+++ src/routes/(main)/+page.svelte
@@ -1,8 +1,7 @@
-<script>
-</script>
-
-<div class="h-screen flex flex-col justify-center shadow-lg">
-	<div class="border border-y border-orange-800 py-8 px-8 md:px-16 space-y-2 text-white bg-orange-500">
+<div class="h-screen flex flex-col justify-center">
+	<div
+		class="border border-y border-orange-800 py-8 px-8 md:px-16 space-y-2 text-white bg-orange-500 shadow-xl"
+	>
 		<h1 class="text-3xl md:text-4xl font-semibold">Deep Creek Bears 2024</h1>
 		<p class="text-xl">More info coming soon...</p>
 	</div>
