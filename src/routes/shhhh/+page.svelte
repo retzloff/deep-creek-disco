6bb38e92fb4383ad14da5a4d65a33643665c8801
Eric Retzloff <eretzloff@gmail.com>
2024-10-23 13:26:41 -0400

    gate page formatting

--- src/routes/shhhh/+page.svelte
+++ src/routes/shhhh/+page.svelte
@@ -1,6 +1,8 @@
 <div
 	style="position: absolute; inset: 0; background: #1e2b43; color: #fff; display: grid; place-items: center; font-family:Arial, Helvetica, sans-serif;"
 >
-	<h1>Gate Code</h1>
-	<h2>9191#</h2>
+	<div>
+		<h1>Gate Code</h1>
+		<h2>9191#</h2>
+	</div>
 </div>
