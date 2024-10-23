907c082e98b656a1d0d1d7e76330a2e5e1ce377a
Eric Retzloff <eretzloff@gmail.com>
2024-10-23 13:12:37 -0400

    updated gate code

--- src/routes/shhhh/+page.svelte
+++ src/routes/shhhh/+page.svelte
@@ -1,5 +1,6 @@
 <div
 	style="position: absolute; inset: 0; background: #1e2b43; color: #fff; display: grid; place-items: center; font-family:Arial, Helvetica, sans-serif;"
 >
-	<h1>#9191</h1>
+	<h1>Gate Code</h1>
+	<h2>9191#</h2>
 </div>
