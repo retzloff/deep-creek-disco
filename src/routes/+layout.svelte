f3258914bf474104d134fe5a53621879aef7e69a
Eric Retzloff <eretzloff@gmail.com>
2024-12-02 00:27:22 -0500

    Set the site name

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,10 @@
 <script>
-	import '../app.css';
-	let { children } = $props();
+  import '../app.css';
+  let { children } = $props();
 </script>
+
+<svelte:head>
+  <title>Deep Creek Bears 2025</title>
+</svelte:head>
 
 {@render children()}
